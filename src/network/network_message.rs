f8da3996a042de57394ecb97a9202152ff34b5e7
Freddie Brown <fred@noser.net>
2021-08-02 00:33:15 +0100

    created NetworkMessage

--- src/network/network_message.rs
+++ src/network/network_message.rs
@@ -1,5 +1,8 @@
+///! Functionality for creating sendable messages across network.
 use crate::blockchain::Event;
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use tokio::io::AsyncReadExt;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct NetworkMessage {
@@ -11,7 +14,47 @@ impl NetworkMessage {
         Self { data }
     }
 
-    // Function to convert struct to byte ready version to send
+    /// Reads a `NetworkMessage` from a async stream
+    pub async fn from_stream<R: AsyncReadExt + Send + Unpin>(
+        stream: &mut R,
+        mut buffer: &mut [u8],
+    ) -> Result<Self> {
+        // Read the size of the message
+        let mut size_buffer = [0_u8; 4];
+        stream.read_exact(&mut size_buffer).await?;
+
+        // Convert it to a u32
+        let message_size = u32::from_be_bytes(size_buffer);
+
+        // Read again from the stream, extending the vector if needed
+        let mut bytes = Vec::new();
+        let mut remaining_size = message_size;
+
+        // Enforce only reading the given size
+        let mut truncated = stream.take(u64::from(remaining_size));
+
+        while remaining_size != 0 {
+            let size = truncated.read(&mut buffer).await?;
+            bytes.extend_from_slice(&buffer[..size]);
+            remaining_size -= size as u32;
+        }
+
+        Ok(serde_json::from_slice(&bytes)?)
+    }
+
+    /// Converts a `NetworkMessage` into a sendable form in bytes
+    pub fn as_bytes(&self) -> Vec<u8> {
+        // Convert the message to bytes
+        let bytes = serde_json::to_vec(&self).unwrap();
+
+        // Prepend with the length
+        let length = bytes.len() as u32;
+
+        let mut message = length.to_be_bytes().to_vec();
+        message.extend(bytes);
+
+        message
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
